f558ab25352dceb5d9d130a1c98a345266930a2c
AlexSandoval99 <102924381+AlexSandoval99@users.noreply.github.com>
2024-07-21 01:09:36 -0400

    vercel

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path'); // Asegúrate de importar el módulo path
 const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000; // Usa el puerto proporcionado por el entorno o 3000 para local
